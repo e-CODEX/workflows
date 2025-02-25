bd871fcc0ff51ef5d4d2f699bba54cf6a7c2eb76
karlkasparr <karl.kulesa.ext@EULISA.EUROPA.eu>
2025-02-25 21:28:36 +0100

    Chore: Update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
     rules: {
-      'body-max-line-length': [0, 'always', 0], // Disable line length rule fro Dependabot
-    },
-  };
-  
+        'type-enum': [
+            2,
+            'always',
+            [
+                'Bugfix',
+                'CI',
+                'Cleanup',
+                'Dependencies',
+                'Documentation',
+                'Refactor',
+                'Security',
+                'Tests',
+                'Feature',
+                'Chore',
+            ]
+        ],
+        'type-case': [2, 'always', 'pascal-case'], // Enforce uppercase first letter
+        'subject-case': [2, 'always', 'sentence-case'], // Ensure subject starts with uppercase
+        'header-max-length': [2, 'always', 72],
+        'body-max-line-length': [0, 'always', 0], // Disable line length rule fro Dependabot
+    }
+};
