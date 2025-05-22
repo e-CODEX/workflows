4562ad62ffdc985a204d4262fd684060df5eb241
karlkasparr <karl.kulesa.ext@EULISA.EUROPA.eu>
2025-05-22 11:21:05 +0200

    feat: add .gitignore & update config files formatting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'body-max-line-length': [0, 'always', 0], // Disable line length rule fro Dependabot
-    },
-  };
-  
+  extends: ['@commitlint/config-conventional'],
+}
+
+// To be used in the future
+// module.exports = {
+//     extends: ['@commitlint/config-conventional'],
+//     rules: {
+//         'body-max-line-length': [0, 'always', 0], // Disable line length rule fro Dependabot
+//     },
+// }
